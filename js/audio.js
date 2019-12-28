b1c07231ca0d312ff3df9502fa3ff90db7cdbae2
Saket Roy <saket.roy@hotmail.com>
2019-12-28 14:42:37 +0530

    Begin to extract fft data from audio

--- js/audio.js
+++ js/audio.js
@@ -1,23 +1,33 @@
 
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-//Create new Audio Context
-const audioContext = new AudioContext();
-
 //Get Canvas Context
 const canvas = document.getElementById("canvas");
-const canvasContext = canvas.getContext('2d');
+const canvasCtx = canvas.getContext('2d');
+
+//Create canvas
+canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+//Create new Audio Context
+const audioCtx = new (window.AudioContext || window.webkitAudioContext);
 
 //To get the audio Element
 const audioElem = document.querySelector("audio");
 
-//Pass it to the Audio Context
-const track = audioContext.createMediaElementSource(audioElem);
-track.connect(audioContext.destination);
 
-//Create canvas
-canvasContext.fillStyle = 'rgb(0, 0, 0)';
-canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+//Create Analyser Node to extract data from Audio Source
+const analyser = audioCtx.createAnalyser();
+analyser.fftSize = 256;
+
+//Pass it to the Audio Context
+const source = audioCtx.createMediaElementSource(audioElem);
+source.connect(audioCtx.destination);
+source.connect(analyser);
+
+const bufferLength = analyser.frequencyBinCount;
+let dataArray = new Uint8Array(bufferLength);
+
+
 
 //Add the Play/Pause Functionality
 const playButton = document.querySelector("button");
@@ -25,9 +35,9 @@ const playButton = document.querySelector("button");
 playButton.addEventListener('click', ()=>{
 
     //Chrome policy: suspended state = Autoplay is denied
-    //Need to resume audioContext initially
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
+    //Need to resume audioCtx initially
+    if (audioCtx.state === 'suspended') {
+        audioCtx.resume();
     }
 
     //PLAY/PAUSE based on current state
@@ -45,5 +55,6 @@ playButton.addEventListener('click', ()=>{
 audioElem.addEventListener('ended', ()=> {
     playButton.dataset.playing = 'false';
 }, false);
+
 
 
