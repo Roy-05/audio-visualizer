57a6d4a572ffd90733973a297165527ec504bb09
Saket Roy <saket.roy@hotmail.com>
2019-12-29 13:11:59 +0530

    Initial Visualization Complete

--- js/audio.js
+++ js/audio.js
@@ -1,37 +1,26 @@
+//Global Variables:
+const canvas = document.getElementById("canvas"),
+    audioCtx = new (window.AudioContext || window.webkitAudioContext),
+    audioElem = document.querySelector("audio"),
+    playButton = document.querySelector("button");
 
-//Get Canvas Context
-const canvas = document.getElementById("canvas");
-const canvasCtx = canvas.getContext('2d');
+let drawVisual,
+    WIDTH = canvas.width;
+    HEIGHT = canvas.height,
+    analyser = audioCtx.createAnalyser();
+
 
 //Create canvas
+const canvasCtx = canvas.getContext('2d');
 canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-
-//Create new Audio Context
-const audioCtx = new (window.AudioContext || window.webkitAudioContext);
-
-//To get the audio Element
-const audioElem = document.querySelector("audio");
-
-
-//Create Analyser Node to extract data from Audio Source
-const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 256;
-
-//Pass it to the Audio Context
 const source = audioCtx.createMediaElementSource(audioElem);
-source.connect(audioCtx.destination);
 source.connect(analyser);
-
-const bufferLength = analyser.frequencyBinCount;
-let dataArray = new Uint8Array(bufferLength);
-
+analyser.connect(audioCtx.destination);
 
 
 //Add the Play/Pause Functionality
-const playButton = document.querySelector("button");
-
 playButton.addEventListener('click', ()=>{
 
     //Chrome policy: suspended state = Autoplay is denied
@@ -44,6 +33,7 @@ playButton.addEventListener('click', ()=>{
     if (playButton.dataset.playing === 'false') {
         audioElem.play();
         playButton.dataset.playing =  'true';
+        visualize()
     }
     else if (playButton.dataset.playing === 'true') {
         audioElem.pause();
@@ -57,4 +47,38 @@ audioElem.addEventListener('ended', ()=> {
 }, false);
 
 
+function visualize() {
+    
+    //Create Analyser Node to extract data from Audio Source
+    analyser.fftSize = 256;
+    
+    const bufferLength = analyser.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
 
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    const draw = () => {
+        drawVisual = requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+        console.log(dataArray);
+
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        let barWidth = (WIDTH/bufferLength) * 2.5,
+            barHeight,
+            x = 0;
+        
+        for(let i = 0; i<bufferLength; i++) {
+            barHeight = dataArray[i];
+            
+            canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ', 50, 50)';
+            canvasCtx.fillRect(x, HEIGHT - barHeight/2, barWidth, barHeight/2);
+            
+            x += barWidth + 1;
+        }
+    };
+
+    draw();
+}
