9200792a3e42b43d7ebde9447fc04a94a4203be3
Saket Roy <saket.roy@hotmail.com>
2020-01-07 00:49:34 +0530

    Make rounded bars

--- js/audio.js
+++ js/audio.js
@@ -74,10 +74,7 @@ function visualize() {
             let point = [WIDTH/2+Math.cos(degree*Math.PI/180)*150,HEIGHT/2+Math.sin(degree*Math.PI/180)*150];
             points[i] = point;
             degree += 360/numOfBars;
-    
         }
-
-        console.log(points);
 
         degree = 225;
         for(let i = 0; i<numOfBars; i++) {
@@ -87,7 +84,8 @@ function visualize() {
             canvasCtx.translate(points[i][0], points[i][1]);
             canvasCtx.rotate(degree*Math.PI/180);
             canvasCtx.fillStyle = 'rgb(255, ' + Math.ceil(i*255/numOfBars) + ', 0)'; 
-            canvasCtx.fillRect(0, 0, barWidth, barHeight);
+   
+            roundRect(canvasCtx, 0, 0, barWidth, barHeight, 5, true)
             
             degree+=360/numOfBars;
             canvasCtx.restore();
@@ -142,3 +140,41 @@ audioElem.addEventListener('ended', ()=> {
     playButton.dataset.playing = 'false';
     visualize()
 }, false);
+
+/**
+ * Source: https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas
+ * by Juan Mendes. Edited by Me for the specific use-case.
+ * 
+ * Draws a rounded rectangle using the current state of the canvas.
+ * If you omit the last three params, it will draw a rectangle
+ * outline with a 5 pixel border radius
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {Number} x The top left x coordinate
+ * @param {Number} y The top left y coordinate
+ * @param {Number} width The width of the rectangle
+ * @param {Number} height The height of the rectangle
+ * @param {Number} [radius = 5] The corner radius; It can also be an object 
+ *                 to specify different radii for corners
+ * @param {Number} [radius.tl = 0] Top left
+ * @param {Number} [radius.tr = 0] Top right
+ * @param {Number} [radius.br = 0] Bottom right
+ * @param {Number} [radius.bl = 0] Bottom left
+ * @param {Boolean} [fill = false] Whether to fill the rectangle.
+ */ 
+function roundRect(ctx, x, y, width, height, radius, fill) {
+    
+    radius = {tl: radius, tr: radius, br: radius, bl: radius};
+    
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    ctx.fill();
+  }
